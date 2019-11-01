92af0c5c9bf36f37ff01de8291e6bfc1a1009637
Wesley Gonçalves Luciano <47697477+wesleylg01@users.noreply.github.com>
2019-11-01 13:56:54 -0300

    add tables categoria and tamanho

--- Pizzaria.sql
+++ Pizzaria.sql
@@ -22,13 +22,20 @@ CREATE TABLE CAIXA (IdCaixa			INT IDENTITY(1,1) PRIMARY KEY,
 					DtHrFechamento	DATETIME,
 					Total_Dia		DECIMAL(10,2),
 					IdUsr			INT FOREIGN KEY REFERENCES USUARIO(IdUsuario) NOT NULL)
-					
-CREATE TABLE PRODUTO (IdProduto   INT IDENTITY(1,1) PRIMARY KEY,
-					  Unidade     VARCHAR(20)   NOT NULL,
-					  Nome        VARCHAR(100)  NOT NULL,
-					  Cod_Barras  VARCHAR(13),
-					  Valor       DECIMAL(10,2) NOT NULL,
-					  Qtd_Estoque DECIMAL(10,3) NOT NULL)
+
+CREATE TABLE CATEGORIA (IdCategoria INT IDENTITY(1,1) PRIMARY KEY,
+                        Nome        VARCHAR(100))
+
+CREATE TABLE TAMANHO (IdTamanho INT IDENTITY(1,1) PRIMARY KEY,
+                      Nome      VARCHAR(100))
+
+CREATE TABLE PRODUTO (IdProduto    INT IDENTITY(1,1) PRIMARY KEY,
+					  Unidade      VARCHAR(20)   NOT NULL,
+					  Nome         VARCHAR(100)  NOT NULL,
+					  Cod_Barras   VARCHAR(13),
+					  MateriaPrima BIT DEFAULT (0),
+					  Valor        DECIMAL(10,2) NOT NULL,
+					  Qtd_Estoque  DECIMAL(10,3) NOT NULL)
 
 CREATE TABLE PEDIDO (IdPedido		 INT IDENTITY(1,1) PRIMARY KEY,
                      Situacao_Pedido BIT DEFAULT (0),
@@ -45,7 +52,9 @@ CREATE TABLE PRATO (IdPrato		INT IDENTITY(1,1) PRIMARY KEY,
 					Nome		VARCHAR(100)  NOT NULL,
 					Custo		DECIMAL(10,2) NOT NULL,
 					Margem		DECIMAL(10,2),
-					Valor_Final DECIMAL(10,2) NOT NULL)
+					Valor_Final  DECIMAL(10,2) NOT NULL,
+					IdCategoria INT FOREIGN KEY REFERENCES CATEGORIA (IdCategoria) NOT NULL,
+					IdTamanho   INT FOREIGN KEY REFERENCES TAMANHO (IdTamanho) NOT NULL)
 
 CREATE TABLE ITENSPRATO(IdItensPrato INT IDENTITY(1,1) PRIMARY KEY,
 						Qtd          DECIMAL(10,3) NOT NULL,
